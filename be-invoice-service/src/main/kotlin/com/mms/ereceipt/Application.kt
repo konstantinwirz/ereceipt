19ba22a7c6bb8c80a6bfc9103d27dfb00d49b10e
Konstantin Wirz <konstantin.wirz@gmail.com>
2019-01-14 20:08:09 +0100

    Write prepared invoice event to the output topic

--- be-invoice-service/src/main/kotlin/com/mms/ereceipt/Application.kt
+++ be-invoice-service/src/main/kotlin/com/mms/ereceipt/Application.kt
@@ -1,19 +1,18 @@
 package com.mms.ereceipt
 
+import com.mms.ereceipt.avro.Affiliate
+import com.mms.ereceipt.avro.InvoicePreparedEvent
 import com.mms.ereceipt.avro.InvoiceRequestedEvent
-import io.confluent.kafka.serializers.KafkaAvroDeserializer
-import io.confluent.kafka.serializers.KafkaAvroSerializer
-import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde
-import org.apache.kafka.common.serialization.Deserializer
-import org.apache.kafka.common.serialization.Serde
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde
 import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.common.serialization.Serializer
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.kstream.Consumed
+import org.apache.kafka.streams.kstream.Produced
 import java.util.*
 
 object Application {
+
 
     val inputTopic = "ereceipt-invoice-requested-events"
     val outputTopic = "ereceipt-invoice-prepared-events"
@@ -22,22 +21,44 @@ object Application {
     @JvmStatic
     fun main(args: Array<String>) {
         val streamBuilder = StreamsBuilder()
-        val avroSerde = GenericAvroSerde().apply {
+        val invoiceRequestedSerde = SpecificAvroSerde<InvoiceRequestedEvent>().apply {
             configure(
-                mapOf("schema.registry.url" to "http://legend:8081")
-                , false
+                mapOf("schema.registry.url" to "http://legend:8081"),
+                false
+            )
+        }
+
+        val invoicePreparedSerde = SpecificAvroSerde<InvoicePreparedEvent>().apply {
+            configure(
+                mapOf("schema.registry.url" to "http://legend:8081"),
+                false
             )
         }
 
         val inputEventStream = streamBuilder.stream<String, InvoiceRequestedEvent>(
             inputTopic,
-            Consumed.with(Serdes.String(), InvoiceRequestedEventSerde())
+            Consumed.with(Serdes.String(), invoiceRequestedSerde)
         )
 
-        inputEventStream.foreach { country, event ->
-            println("COUNTRY = $country")
+        val mapValues = inputEventStream.mapValues { country, event ->
             println("EVENT = $event")
+            InvoicePreparedEvent.newBuilder()
+                .setId(event.id)
+                .setType(event.type)
+                .setAmount(event.amount)
+                .setCurrency(event.currency)
+                .setCountry(event.country)
+                .setAffiliate(
+                    Affiliate.newBuilder().setKeyIndex(event.affiliate.keyIndex).setAlgo(event.affiliate.algo).setPayload(
+                        event.affiliate.payload
+                    ).build()
+                )
+                .setLines(event.lines)
+                .build()
         }
+
+        mapValues.to(outputTopic, Produced.with(Serdes.String(), invoicePreparedSerde))
+
 
         val props = Properties()
         props["bootstrap.servers"] = "legend:29092"
@@ -46,44 +67,4 @@ object Application {
         val streams = KafkaStreams(topology, props)
         streams.start()
     }
-}
-
-
-class InvoiceRequestedEventSerializer : Serializer<InvoiceRequestedEvent> {
-    val avroSerializer = KafkaAvroSerializer()
-
-    override fun configure(configs: MutableMap<String, *>?, isKey: Boolean) =
-        avroSerializer.configure(configs, isKey)
-
-    override fun serialize(topic: String?, data: InvoiceRequestedEvent?): ByteArray =
-        avroSerializer.serialize(topic, data)
-
-    override fun close() =
-        avroSerializer.close()
-}
-
-class InvoiceRequestedEventDeserializer : Deserializer<InvoiceRequestedEvent> {
-    val avroDeserializer = KafkaAvroDeserializer()
-
-    override fun configure(configs: MutableMap<String, *>?, isKey: Boolean) =
-        avroDeserializer.configure(configs, isKey)
-
-    override fun deserialize(topic: String?, data: ByteArray?): InvoiceRequestedEvent =
-        avroDeserializer.deserialize(topic, data) as InvoiceRequestedEvent
-
-    override fun close() =
-        avroDeserializer.close()
-}
-
-class InvoiceRequestedEventSerde : Serde<InvoiceRequestedEvent> {
-
-    override fun configure(configs: MutableMap<String, *>?, isKey: Boolean) {}
-
-    override fun deserializer(): Deserializer<InvoiceRequestedEvent> =
-        InvoiceRequestedEventDeserializer()
-
-    override fun close() {}
-
-    override fun serializer(): Serializer<InvoiceRequestedEvent> =
-        InvoiceRequestedEventSerializer()
 }
